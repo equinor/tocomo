e976b7c18cbdf9b9ad7b922892f535923e86ff1d
Lars Petter Hauge <larsenhauge@gmail.com>
2024-06-21 09:19:06 +0200

    Fix query call from frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState('')
 
   useEffect(() => {
-    fetch(`http://localhost:5005/run_reactions?=H2O=30&O2=10&SO2=0&NO2=1.5&H2S=3&H2SO4=0&HNO3=0&NO=0`)
+    fetch(`http://localhost:5005/run_reactions?H2O=30&O2=10&SO2=0&NO2=1.5&H2S=3&H2SO4=0&HNO3=0&NO=0`)
       .then(response => response.json())
       .then(data => setMessage(data.H2O));
   }, []);
