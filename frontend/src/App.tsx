b9a42a22b781e2bef09781e58b9e43d14e11951c
Olve Maudal <olvm@equinor.com>
2024-06-25 22:58:27 +0200

    Try to use the reverse proxy again

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,8 +22,8 @@ const defaultValues: ChemicalValues = {
   NO: 0,
 };
 
-const baseURL: string = `https://backend-c2d2-web-portal-test-dev.playground.radix.equinor.com`
-//const baseURL: string = `https://frontend-c2d2-web-portal-test-dev.playground.radix.equinor.com`
+//const baseURL: string = `https://backend-c2d2-web-portal-test-dev.playground.radix.equinor.com`
+const baseURL: string = `https://frontend-c2d2-web-portal-test-dev.playground.radix.equinor.com`
 //const baseURL: string = `http://localhost:5005`
 //const baseURL: string = `http://localhost:3000`
 //const baseURL: string = ""
